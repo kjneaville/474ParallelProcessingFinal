22f4f93f799d3cc3b81c1bddeb42ad13085ede3b
Jason Ho <raktos35@gmail.com>
2016-05-27 19:35:40 -0700

    stub instruction interleaving

--- js/dataRaces.js
+++ js/dataRaces.js
@@ -3,5 +3,23 @@
  */
 
 $(document).ready(function() {
-    
+    // create data
+    var instructions = [
+        {align: 'left', instruction: 'instruction 1'},
+        {align: 'left', instruction: 'instruction 2'},
+        {align: 'right', instruction: 'instruction 1'},
+        {align: 'left', instruction: 'instruction 3'},
+        {align: 'right', instruction: 'instruction 2'},
+        {align: 'left', instruction: 'instruction 4'},
+        {align: 'right', instruction: 'instruction 3'},
+        {align: 'right', instruction: 'instruction 4'}
+    ];
+
+    var parentDiv = d3.select('#race-anim');
+    var paragraphs = parentDiv.selectAll('p.instruction').data(instructions, function(d) {return instructions.indexOf(d);});
+
+    paragraphs.enter().append('p')
+        .attr('class', 'instruction')
+        .text(function(d) {return d.instruction;})
+        .style('text-align', function(d) {return d.align;});
 });
